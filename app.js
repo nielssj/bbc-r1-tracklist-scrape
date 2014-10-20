8897ec70a4542916422ece61186806b82dc7f894
Niels Søholm <nm@9la.dk>
2014-10-20 15:09:17 +0800

    First and most basic implementation able to scrape from a file. Exported as a node module with a single function

---
+++ app.js
@@ -0,0 +1,34 @@
+var fs = require("fs");
+var cheerio = require("cheerio");
+
+// Scrape tracks from a file
+exports.scrapeFromFile = function(filename, encoding, callback) {
+    fs.readFile('tracklist.htm', 'utf8', function (err,data) {
+        // Abort in case of IO error
+        if (err) {
+            console.log(err);
+            callback(null, err);
+            return;
+        }
+
+        // Parse HTML into Cheerio
+        var $ = cheerio.load(data);
+
+        // Scrape track information
+        tracks = [];
+        var tracksSelect = $(".segment__track");
+        tracksSelect.each(function (i, elem) {
+            track = {
+                artist: $(this).find("span .artist").html(),
+                title: $(this).find("p[property=name]").html(),
+                contributors: $(this).find("span[property=contributor] span").html(),
+                publisher: $(this).find("abbr[property=publisher] span").html()
+            };
+            tracks.push(track);
+        });
+
+        // Return full track list through callback
+        console.log(tracks);
+        callback(tracks);
+    });
+};
